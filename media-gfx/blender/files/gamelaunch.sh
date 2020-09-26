5f363f9d18038c312e25471bcb495a7ac1e55146
Orson Teodoro <orsonteodoro@hotmail.com>
2020-09-25 17:28:59 -0700

    Clarify the comment in gamelaunch.sh in media-gfx/blender

--- media-gfx/blender/files/gamelaunch.sh
+++ media-gfx/blender/files/gamelaunch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 pushd $(dirname "${BASH_SOURCE[0]}") 2>/dev/null 1>/dev/null
 
-# LIBGL_DRIVERS_DIR is deprecated and works in LLVM-9 linked, but
+# LIBGL_DRIVERS_DIR is deprecated and works with linked LLVM-9, but
 # LIBGL_DRIVERS_PATH does not work but added for the future.
-# This is to avoid multiple llvm problem.  This should match the LLVM-9 linked
-# {...}_dri.so drivers.
+# This is to avoid multiple llvm problem.  This should match the LLVM-9 (or) the
+# version linked to the same LLVM version in Blender and {...}_dri.so drivers
+# needs to match that version.
 export LIBGL_DRIVERS_DIR="$(pwd)/lib/dri"
 export LIBGL_DRIVERS_PATH="$(pwd)/lib/dri"
 
