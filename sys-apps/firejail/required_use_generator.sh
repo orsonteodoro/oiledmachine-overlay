2a6b4567066867f4481cc7aac18a474d593a7e31
Orson Teodoro <orsonteodoro@hotmail.com>
2022-02-23 11:27:33 -0800

    Update the firejail ebuild to 0.9.68

--- sys-apps/firejail/required_use_generator.sh
+++ sys-apps/firejail/required_use_generator.sh
@@ -6,13 +6,13 @@
 # #1 identify and find all the nodes (aka points)
 # #2 connect the edges via conditional dependency
 
-ETC_FOLDER="/var/tmp/portage/sys-apps/firejail-0.9.66/work/firejail-0.9.66/etc"
+ETC_FOLDER="/var/tmp/portage/sys-apps/firejail-0.9.68/work/firejail-0.9.68/etc"
 
 unset nodes
 nodes=()
 
 for f in $(find "${ETC_FOLDER}/"{profile-a-l,profile-m-z} -name "*.profile") ; do
-	n=$(basename "${f}" | sed -e "s|.profile||g")
+	n=$(basename "${f}" | sed -e "s|.profile||g" | sed -e "s|\.|_|g")
 	nodes+=(${n})
 done
 
@@ -22,8 +22,9 @@ declare -A g
 # The parent is always left hand side, the child is the right hand side in the bucket list
 
 for p in $(echo "${nodes[@]}" | tr " " "\n" | sort) ; do
-	profiles=$(grep -E -e "include .*.profile" $(ls "${ETC_FOLDER}/"*/${p}.profile))
-	childs=$(echo "${profiles}" | sed -e "s|.profile||" -e "s|include ||")
+	ls "${ETC_FOLDER}/"*/${p}.profile 2>/dev/null 1>/dev/null || continue
+	profiles=$(grep -E -e "include .*.profile" $(ls "${ETC_FOLDER}/"*/${p}.profile) | sed -e "/^#/d")
+	childs=$(echo "${profiles}" | sed -e "s|\.profile||" -e "s|include ||" -e "s|\.|_|g")
 	if [[ -n "${childs}" ]] ; then
 		childs=$(echo "${childs}" | sed -e "s|^|firejail_profiles_|g")
 		echo -e "\tfirejail_profiles_${p}? ( ${childs} )"  # parent: childs
