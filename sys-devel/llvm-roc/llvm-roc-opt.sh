e8dee0b77c5e91805832f487704a7a46ae64d0c7
Orson Teodoro <orsonteodoro@hotmail.com>
2023-10-17 08:46:17 -0700

    Combine the t script and allow to build all slots in llvm-roc-opt.sh.  Update documentation in metadata.xml.

--- sys-devel/llvm-roc/llvm-roc-opt.sh
+++ sys-devel/llvm-roc/llvm-roc-opt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 _src_train() {
-	LLVM_ROC_PGO_TRAINING=1
+	export LLVM_ROC_PGO_TRAINING=1
 	if [[ -e "${ROCM_OVERLAY_DIR}/sci-libs/composable_kernel" && "${LLVM_ROC_TRAINERS}" =~ "composable_kernel" && -n "${COMPOSABLE_KERNEL_PGO_TRAINING_USE}" ]] ; then
 		pushd "${ROCM_OVERLAY_DIR}/sci-libs/composable_kernel"
 			USE="${ROCBLAS_PGO_TRAINING_USE}" ebuild composable_kernel-${ROCM_SLOT}*.ebuild digest clean unpack prepare compile
@@ -35,40 +35,61 @@ _src_train() {
 	unset LLVM_ROC_PGO_TRAINING
 }
 
-main() {
-	LLVM_ROC_EPGO=${LLVM_ROC_EPGO:-"1"}
-	LLVM_ROC_EBOLT=${LLVM_ROC_EBOLT:-"0"}
+die() {
+	msg="${@}"
+	echo "error:  ${msg}"
+	exit 1
+}
+
+_build_one_slot() {
 	if [[ -z "${ROCM_SLOT}" ]] ; then
 echo "ROCM_SLOT must be defined as an environment variable."
 		exit 1
 	fi
-	if [[ -z "${ROCM_OVERLAY_DIR}" ]] ; then
-echo "ROCM_OVERLAY_DIR must be defined as an environment variable."
-		exit 1
-	fi
+echo "Building sys-devel/llvm-roc:${ROCM_SLOT}"
 
 	if [[ "${LLVM_ROC_EPGO}" == "1" ]] ; then
 echo "PGI Phase (1/3)"
-		USE="epgo" emerge llvm-roc:${ROCM_SLOT}
+		USE="epgo" emerge llvm-roc:${ROCM_SLOT} || die "Encountered build failure.  PGI failed"
 
 echo "PGT Phase (2/3)"
 		_src_train
 
 echo "PGO Phase (3/3)"
-		USE="epgo" emerge llvm-roc:${ROCM_SLOT}
+		USE="epgo" emerge llvm-roc:${ROCM_SLOT} || die "Encountered build failure.  PGO failed"
 	fi
 
 	if [[ "${LLVM_ROC_EBOLT}" == "1" ]] ; then
 
 echo "BGI Phase (1/3)"
-		USE="epgo ebolt" emerge llvm-roc:${ROCM_SLOT}
+		USE="epgo ebolt" emerge llvm-roc:${ROCM_SLOT} || die "Encountered build failure.  BGI failed"
 
 echo "BGT Phase (2/3)"
 		_src_train
 
 echo "BGO Phase (3/3)"
-		USE="epgo ebolt" emerge llvm-roc:${ROCM_SLOT}
+		USE="epgo ebolt" emerge llvm-roc:${ROCM_SLOT} || die "Encountered build failure.  BGO failed"
 	fi
+
+}
+
+main() {
+	LLVM_ROC_ENV_PATH=${LLVM_ROC_ENV_PATH:-"/etc/portage/env/llvm-roc.conf"}
+	source "${LLVM_ROC_ENV_PATH}"
+	LLVM_ROC_EPGO=${LLVM_ROC_EPGO:-"1"}
+	LLVM_ROC_EBOLT=${LLVM_ROC_EBOLT:-"0"}
+	ROCM_SLOTS=${ROCM_SLOTS:-"5.1 5.2 5.3 5.4 5.5 5.6 5.7"}
+
+	if [[ -z "${ROCM_OVERLAY_DIR}" ]] ; then
+echo "ROCM_OVERLAY_DIR must be defined as an environment variable."
+		exit 1
+	fi
+
+	local s
+	for s in ${ROCM_SLOTS} ; do
+		ROCM_SLOT="${s}"
+		_build_one_slot
+	done
 }
 
 main
