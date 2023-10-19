11423bae02c43a25503a75e7f45e3a03dae45599
Orson Teodoro <orsonteodoro@hotmail.com>
2023-10-18 17:41:35 -0700

    Update optimize.sh for trainers for clang ebuild.  Update documentation in metadata.xml.

--- sys-devel/clang/optimize.sh
+++ sys-devel/clang/optimize.sh
@@ -4,16 +4,135 @@
 # ebuild command inside the .ebuild even within a wrapper script in the ebuild.
 # This script fixes that.
 
+# For system-llvm USE flag
+_get_rocm_slot() {
+	local clang_slot="${1}"
+	local rocm_slot=""
+	local ROCM_SLOTS=(5_1 5_2 5_3 5_4 5_5 5_6 5_7)
+	local s
+	for s in ${ROCM_SLOTS[@]} ; do
+		if grep "rocm_${s}" /var/db/pkg/sys-devel/clang-${clang_slot}*/USE ; then
+			rocm_slot="${s}"
+			break
+		fi
+	done
+	echo "${rocm_slot}"
+}
+
+ver_eq() {
+	local a="${1}"
+	local b="${2}"
+	[[ "${a}" -eq "${b}" ]] && return 0
+	return 1
+}
+
+ver_ne() {
+	local a="${1}"
+	local b="${2}"
+	[[ "${a}" -ne "${b}" ]] && return 0
+	return 1
+}
+
+ver_lt() {
+	local a="${1}"
+	local b="${2}"
+	local r=$(echo -e "${a}\n${b}" | sort -V | tail -n 1)
+	[[ "${a}" -eq "${b}" ]] && return 1
+	[[ "${r}" -eq "${b}" ]] && return 0
+	return 1
+}
+
+ver_lt() {
+	local a="${1}"
+	local b="${2}"
+	local r=$(echo -e "${a}\n${b}" | sort -V | tail -n 1)
+	[[ "${a}" -eq "${b}" ]] && return 0
+	[[ "${r}" -eq "${b}" ]] && return 0
+	return 1
+}
+
+ver_gt() {
+	local a="${1}"
+	local b="${2}"
+	local r=$(echo -e "${a}\n${b}" | sort -V | tail -n 1)
+	[[ "${a}" -eq "${b}" ]] && return 1
+	[[ "${r}" -eq "${a}" ]] && return 0
+	return 1
+}
+
+ver_ge() {
+	local a="${1}"
+	local b="${2}"
+	local r=$(echo -e "${a}\n${b}" | sort -V | tail -n 1)
+	[[ "${a}" -eq "${b}" ]] && return 0
+	[[ "${r}" -eq "${a}" ]] && return 0
+	return 1
+}
+
 _src_train() {
 	export CLANG_PGO_TRAINING=1
 	export CC="clang-${CLANG_SLOT}"
 	export CXX="clang++${CLANG_SLOT}"
+	local pn
 # TODO:  Add more ebuilds
 	if [[ -e "${OVERLAY_DIR}/sys-devel/lld" && "${CLANG_TRAINERS}" =~ "lld" ]] ; then
 		pushd "${OVERLAY_DIR}/sys-devel/lld"
 			ebuild lld-${CLANG_SLOT}*.ebuild digest clean unpack prepare compile
 		popd
 	fi
+	if [[ -e "${OVERLAY_DIR}/media-video/ffmpeg" && "${CLANG_TRAINERS}" =~ "ffmpeg" ]] ; then
+		pushd "${OVERLAY_DIR}/media-video/ffmpeg"
+			local fn=$(ls 6.0* | sort -V | tail -n 1)
+			if [[ -e "${fn}" ]] ; then
+				ebuild ${fn} digest clean unpack prepare compile
+			fi
+		popd
+	fi
+
+	if [[ -e "${OVERLAY_DIR}/media-libs/mesa" && "${CLANG_TRAINERS}" =~ "mesa" ]] && ver_le "${CLANG_SLOT}" "16" ; then
+		pushd "${OVERLAY_DIR}/media-libs/mesa"
+			local fn=$(ls 23.2* | sort -V | tail -n 1)
+			if [[ -e "${fn}" ]] ; then
+				ebuild ${fn} digest clean unpack prepare compile
+			fi
+		popd
+	fi
+
+	local ROCM_SLOT=$(_get_rocm_slot "${CLANG_SLOT}")
+	if [[ -e "${OVERLAY_DIR}/sci-libs/composable_kernel" && "${CLANG_TRAINERS}" =~ "composable_kernel" ]] ; then
+		pushd "${OVERLAY_DIR}/sci-libs/composable_kernel"
+			local fn=$(ls *6.0* | sort -V | tail -n 1)
+			if [[ -e "${fn}" ]] ; then
+				ebuild composable_kernel-${ROCM_SLOT}*.ebuild digest clean unpack prepare compile
+			fi
+		popd
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocBLAS" && "${CLANG_TRAINERS}" =~ "rocBLAS" ]] ; then
+		pushd "${OVERLAY_DIR}/sci-libs/rocBLAS"
+			ebuild rocBLAS-${ROCM_SLOT}*.ebuild digest clean unpack prepare compile
+		popd
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocMLIR" && "${CLANG_TRAINERS}" =~ "rocMLIR" ]] ; then
+		pushd "${OVERLAY_DIR}/sci-libs/rocMLIR"
+			ebuild rocMLIR-${ROCM_SLOT}*.ebuild digest clean unpack prepare compile
+		popd
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocPRIM" && "${CLANG_TRAINERS}" =~ "rocPRIM" ]] ; then
+		pushd "${OVERLAY_DIR}/sci-libs/rocPRIM"
+			ebuild rocPRIM-${ROCM_SLOT}*.ebuild digest clean unpack prepare compile
+		popd
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocRAND" && "${CLANG_TRAINERS}" =~ "rocRAND" ]] ; then
+		pushd "${OVERLAY_DIR}/sci-libs/rocRAND"
+			ebuild rocRAND-${ROCM_SLOT}*.ebuild digest clean unpack prepare compile
+		popd
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocSPARSE" && "${CLANG_TRAINERS}" =~ "rocSPARSE" ]] ; then
+		pushd "${OVERLAY_DIR}/sci-libs/rocSPARSE"
+			ebuild rocSPARSE-${ROCM_SLOT}*.ebuild digest clean unpack prepare compile
+		popd
+	fi
+
 	unset CLANG_PGO_TRAINING
 }
 
@@ -85,6 +204,31 @@ echo "Checking if *DEPENDs was installed for llvm, clang, lld"
 	emerge -1vo llvm:${CLANG_SLOT} || die "Encountered build failure.  Prereq check/install failed for llvm"
 	emerge -1vo clang:${CLANG_SLOT} || die "Encountered build failure.  Prereq check/install failed for clang"
 	emerge -1vo lld:${CLANG_SLOT} || die "Encountered build failure.  Prereq check/install failed for lld"
+	local ROCM_SLOT=$(_get_rocm_slot "${CLANG_SLOT}")
+	if [[ -e "${OVERLAY_DIR}/media-video/ffmpeg" && "${CLANG_TRAINERS}" =~ "ffmpeg" ]] ; then
+		emerge -1vo =ffmpeg-6.0* || die "Encountered build failure.  Prereq check/install failed for ffmpeg"
+	fi
+	if [[ -e "${OVERLAY_DIR}/media-libs/mesa" && "${CLANG_TRAINERS}" =~ "mesa" ]] ; then
+		emerge -1vo =mesa-23.2* || die "Encountered build failure.  Prereq check/install failed for mesa"
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/composable_kernel" && "${CLANG_TRAINERS}" =~ "composable_kernel" ]] ; then
+		emerge -1vo composable_kernel:${ROCM_SLOT} || die "Encountered build failure.  Prereq check/install failed for composable_kernel:${ROCM_SLOT}"
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocBLAS" && "${CLANG_TRAINERS}" =~ "rocBLAS" ]] ; then
+		emerge -1vo rocBLAS:${ROCM_SLOT} || die "Encountered build failure.  Prereq check/install failed for rocBLAS:${ROCM_SLOT}"
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocMLIR" && "${CLANG_TRAINERS}" =~ "rocMLIR" ]] ; then
+		emerge -1vo rocMLIR:${ROCM_SLOT} || die "Encountered build failure.  Prereq check/install failed for rocMLIR:${ROCM_SLOT}"
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocPRIM" && "${CLANG_TRAINERS}" =~ "rocPRIM" ]] ; then
+		emerge -1vo rocPRIM:${ROCM_SLOT} || die "Encountered build failure.  Prereq check/install failed for rocPRIM:${ROCM_SLOT}"
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocRAND" && "${CLANG_TRAINERS}" =~ "rocRAND" ]] ; then
+		emerge -1vo rocRAND:${ROCM_SLOT} || die "Encountered build failure.  Prereq check/install failed for rocRAND:${ROCM_SLOT}"
+	fi
+	if [[ -e "${OVERLAY_DIR}/sci-libs/rocSPARSE" && "${CLANG_TRAINERS}" =~ "rocSPARSE" ]] ; then
+		emerge -1vo rocSPARSE:${ROCM_SLOT} || die "Encountered build failure.  Prereq check/install failed for rocSPARSE:${ROCM_SLOT}"
+	fi
 }
 
 main() {
