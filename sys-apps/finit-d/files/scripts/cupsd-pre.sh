dc879b7ab13a5c43d5cc7a25a98f16af958d0b7f
Orson Teodoro <orsonteodoro@hotmail.com>
2023-12-30 14:36:00 -0800

    Update cupsd-pre.sh for finit-d ebuild

--- sys-apps/finit-d/files/scripts/cupsd-pre.sh
+++ sys-apps/finit-d/files/scripts/cupsd-pre.sh
@@ -1,23 +1,20 @@
 #/bin/bash
-L=(
-	"d;0775;root:lp;/var/cache/cups"
-	"d;0775;root:lp;/var/cache/cups/rss"
-	"d;0755;root:lp;/run/cups"
-	"d;0511;lp:lpadmin;/run/cups/certs"
-)
-
-local row
-for row in ${L[@]} ; do
-	local type=$(cat "${row}" | cut -f 1 -d ";")
-	local mod=$(cat "${row}" | cut -f 2 -d ";")
-	local owner=$(cat "${row}" | cut -f 3 -d ";")
-	local path=$(cat "${row}" | cut -f 4 -d ";")
-	if [[ "${type}" == "d" && ! -e "${path}" ]] ; then
+get_ready_dir() {
+	local row="${1}"
+	mod=$(echo "${row}" | cut -f 1 -d ";")
+	owner=$(echo "${row}" | cut -f 2 -d ";")
+	path=$(echo "${row}" | cut -f 3 -d ";")
+	if [[ ! -e "${path}" ]] ; then
 		mkdir -p "${path}"
 		chown "${owner}" "${path}"
 		chmod "${mod}" "${path}"
-	elif [[ "${type}" == "d" && -d "${path}" ]] ; then
+	elif [[ -d "${path}" ]] ; then
 		chown "${owner}" "${path}"
 		chmod "${mod}" "${path}"
 	fi
-done
+}
+
+get_ready_dir "0775;root:lp;/var/cache/cups"
+get_ready_dir "0775;root:lp;/var/cache/cups/rss" &
+get_ready_dir "0755;root:lp;/run/cups" &
+get_ready_dir "0511;lp:lpadmin;/run/cups/certs" &
