c699828dd2a2c0771004c26dc01a8e21de70b327
Orson Teodoro <orsonteodoro@hotmail.com>
2023-12-30 14:39:08 -0800

    Fix fork

--- sys-apps/finit-d/files/scripts/cupsd-pre.sh
+++ sys-apps/finit-d/files/scripts/cupsd-pre.sh
@@ -14,7 +14,7 @@ get_ready_dir() {
 	fi
 }
 
-get_ready_dir "0755;root:lp;/run/cups" &
+get_ready_dir "0755;root:lp;/run/cups"
 get_ready_dir "0511;lp:lpadmin;/run/cups/certs" &
 get_ready_dir "0775;root:lp;/var/cache/cups"
 get_ready_dir "0775;root:lp;/var/cache/cups/rss" &
