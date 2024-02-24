8880cf9eb3bb140879656c320a997e8958550611
Orson Teodoro <orsonteodoro@hotmail.com>
2024-02-24 09:30:49 -0800

    Update autobump script for ot-sources ebuild

--- sys-kernel/ot-sources/autobump/custom.sh
+++ sys-kernel/ot-sources/autobump/custom.sh
@@ -55,7 +55,7 @@ main() {
 	local repo_dir=$(realpath $(dirname "${BASH_SOURCE[0]}")"/../../../")
 	local package_dir=$(realpath $(dirname "${BASH_SOURCE[0]}")"/../")
 
-	. description
+	. "${package_dir}/autobump/description"
 
 	# Update sys-kernel/ot-sources
 	#echo "package_dir: ${package_dir}"
@@ -71,85 +71,100 @@ main() {
 		echo "Processing:  ${latest_upstream_version}"
 		local ver_=$(echo "${ver}" | sed -e "s|-r[0-9]$||g")
 		if [[ "${latest_upstream_version}" != "${ver_}" ]] ; then
-			local current_rt_ver=$(get_latest_rt_patchset_version "${slot}")
-			if [[ -n "${current_rt_ver}" ]] ; then
-				echo "Auto bumping -rt patchset to ${current_rt_ver}"
-				local lineno=$(grep -n "PATCH_RT_VER" "${package_dir}/ot-sources-${ver}.ebuild" | cut -f 1 -d ":")
-				if [[ -z "${lineno}" ]] ; then
-					lineno=6
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}i PATCH_RT_VER=\"${current_rt_ver}\""
-				else
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}d"
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}i PATCH_RT_VER=\"${current_rt_ver}\""
-				fi
-			fi
-
-			local current_genpatches_ver=$(get_latest_genpatches_version "${slot}" | cut -f 2 -d "-")
-			if [[ -n "${current_genpatches_ver}" ]] ; then
-				echo "Auto bumping genpatches patchset to ${current_genpatches_ver}"
-				local lineno=$(grep -n "GENPATCHES_VER" "${package_dir}/ot-sources-${ver}.ebuild" | cut -f 1 -d ":")
-				if [[ -z "${lineno}" ]] ; then
-					lineno=6
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}i GENPATCHES_VER=\"${current_genpatches_ver}\""
-				else
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}d"
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}i GENPATCHES_VER=\"${current_genpatches_ver}\""
-				fi
-			fi
-
-			local current_clear_ver=$(get_latest_genpatches_version "${slot}" | cut -f 2 -d "-")
-			if [[ -n "${current_clear_ver}" ]] ; then
-				echo "Auto bumping clear patchset to ${current_clear_ver}"
-				local lineno=$(grep -n "CLEAR_LINUX_PATCHES_VER" "${package_dir}/ot-sources-${ver}.ebuild" | cut -f 1 -d ":")
-				if [[ -z "${lineno}" ]] ; then
-					lineno=6
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}i CLEAR_LINUX_PATCHES_VER=\"${current_clear_ver}\""
-				else
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}d"
-					[[ "${DRY_RUN}" != "1" ]] && sed -i -e "${lineno}i CLEAR_LINUX_PATCHES_VER=\"${current_clear_ver}\""
-				fi
-			fi
-
-			if [[ "${ver_}" != "${latest_upstream_version}" ]] ; then
-				echo "Auto bumping ot-sources-${ver}.ebuild -> ot-sources-${latest_upstream_version}.ebuild"
+			echo "Auto bumping ot-sources-${ver}.ebuild -> ot-sources-${latest_upstream_version}.ebuild"
+			pushd "${repo_dir}/virtual/ot-sources-stable" >/dev/null 2>&1
 				if [[ "${DRY_RUN}" != "1" ]] ; then
 					cp -a \
-						"${package_dir}/ot-sources-${ver}.ebuild" \
-						"${package_dir}/ot-sources-${latest_upstream_version}.ebuild"
+						"ot-sources-${ver}.ebuild" \
+						"ot-sources-${latest_upstream_version}.ebuild"
 				fi
-			fi
+				local current_rt_ver=$(get_latest_rt_patchset_version "${slot}")
+				if [[ -n "${current_rt_ver}" ]] ; then
+					echo "Auto bumping -rt patchset to ${current_rt_ver}"
+					if [[ "${DRY_RUN}" != "1" ]] ; then
+						local lineno=$(grep -n "PATCH_RT_VER" "${package_dir}/ot-sources-${ver}.ebuild" | cut -f 1 -d ":")
+						if [[ -z "${lineno}" ]] ; then
+							lineno=6
+							sed -i -e "${lineno}i PATCH_RT_VER=\"${current_rt_ver}\"" "ot-sources-${latest_upstream_version}.ebuild"
+						else
+							sed -i -e "${lineno}d" "ot-sources-${latest_upstream_version}.ebuild"
+							sed -i -e "${lineno}i PATCH_RT_VER=\"${current_rt_ver}\"" "ot-sources-${latest_upstream_version}.ebuild"
+						fi
+					fi
+				fi
+
+				local current_genpatches_ver=$(get_latest_genpatches_version "${slot}" | cut -f 2 -d "-")
+				if [[ -n "${current_genpatches_ver}" ]] ; then
+					echo "Auto bumping genpatches patchset to ${current_genpatches_ver}"
+					if [[ "${DRY_RUN}" != "1" ]] ; then
+						local lineno=$(grep -n "GENPATCHES_VER" "${package_dir}/ot-sources-${ver}.ebuild" | cut -f 1 -d ":")
+						if [[ -z "${lineno}" ]] ; then
+							lineno=6
+							sed -i -e "${lineno}i GENPATCHES_VER=\"${current_genpatches_ver}\"" "ot-sources-${latest_upstream_version}.ebuild"
+						else
+							sed -i -e "${lineno}d" "ot-sources-${latest_upstream_version}.ebuild"
+							sed -i -e "${lineno}i GENPATCHES_VER=\"${current_genpatches_ver}\"" "ot-sources-${latest_upstream_version}.ebuild"
+						fi
+					fi
+				fi
+
+				local current_clear_ver=$(get_latest_clear_version "${slot}")
+				if [[ -n "${current_clear_ver}" ]] ; then
+					echo "Auto bumping clear patchset to ${current_clear_ver}"
+					if [[ "${DRY_RUN}" != "1" ]] ; then
+						local lineno=$(grep -n "CLEAR_LINUX_PATCHES_VER" "${package_dir}/ot-sources-${ver}.ebuild" | cut -f 1 -d ":")
+						if [[ -z "${lineno}" ]] ; then
+							lineno=6
+							sed -i -e "${lineno}i CLEAR_LINUX_PATCHES_VER=\"${current_clear_ver}\"" "ot-sources-${latest_upstream_version}.ebuild"
+						else
+							sed -i -e "${lineno}d" "ot-sources-${latest_upstream_version}.ebuild"
+							sed -i -e "${lineno}i CLEAR_LINUX_PATCHES_VER=\"${current_clear_ver}\"" "ot-sources-${latest_upstream_version}.ebuild"
+						fi
+					fi
+				fi
+
+				if [[ "${DRY_RUN}" != "1" ]] ; then
+					ebuild "ot-sources-${latest_upstream_version}.ebuild" digest
+					git add *
+					git commit -m "Autobumped ot-sources-${latest_upstream_version}.ebuild"
+				fi
+			popd >/dev/null 2>&1
 		fi
 	done
 
 	# Update virtual/ot-sources-stable
-	pushd "${repo_dir}/virtual/ot-sources-stable"
+	pushd "${repo_dir}/virtual/ot-sources-stable" >/dev/null 2>&1
 		for slot in ${STABLE_SLOTS[@]} ; do
 			local latest_stable_ebuild_version=$(ls -1 \
 				| grep -E -e "ot-sources-stable-${slot}\..*\.ebuild$" \
 				| sort -V \
 				| tail -n 1 \
 				| sed -e "s|ot-sources-stable-||" -e "s|\.ebuild$||")
-			local latest_stable_ebuild_version_=$(echo "latest_stable_ebuild_version" | sed -r -e "s|-r[0-9]+||g")
-			pushd "${repo_dir}/sys-kernel/ot-sources/"
+			local latest_stable_ebuild_version_=$(echo "latest_stable_ebuild_version" | sed -r -e "s|-r[0-9]+$||g")
+			pushd "${repo_dir}/sys-kernel/ot-sources/" >/dev/null 2>&1
 				local latest_current_release=$(ls -1 "ot-sources-${slot}."*".ebuild" \
 					| sort -V \
 					| tail -n 1 \
 					| sed -e "s|ot-sources-||g" -e "s|\.ebuild||g")
-				if [[ "${latest_stable_ebuild_version_}" != "${latest_current_release}" ]] ; then
+				local latest_current_release_=$(echo "${latest_current_release}" | sed -r -e "s|-r[0-9]+$||g")
+				if [[ "${latest_stable_ebuild_version_}" != "${latest_current_release_}" ]] ; then
 					echo "Auto bumping ot-sources-stable-${latest_stable_ebuild_version}.ebuild -> ot-sources-stable-${latest_current_release}.ebuild"
 					if [[ "${DRY_RUN}" != "1" ]] ; then
 						cp -a \
 							"${repo_dir}/virtual/ot-sources-stable/ot-sources-stable-${latest_stable_ebuild_version}.ebuild" \
 							"${repo_dir}/virtual/ot-sources-stable/ot-sources-stable-${latest_current_release}.ebuild"
+						pushd "${repo_dir}/virtual/ot-sources-stable" >/dev/null 2>&1
+							if [[ "${DRY_RUN}" != "1" ]] ; then
+								ebuild $(ls -1 *.ebuild | sort -V | tail -n 1) digest
+								git add *
+								git commit -m "Autobumped ot-sources-stable-${latest_current_release}.ebuild"
+							fi
+						popd >/dev/null 2>&1
 					fi
 				fi
-			popd
-			pushd "${repo_dir}/virtual/ot-sources-stable" || popd
-				local ebuild_path=$(ls -1 *.ebuild)
-				ebuild "${ebuild_path}" digest
-			popd
+			popd >/dev/null 2>&1
 		done
-	popd
+	popd >/dev/null 2>&1
 
 	# Update virtual/ot-sources-lts
 	local current_lts_timestamp=$(wget -q -O - "https://www.kernel.org/" \
@@ -159,37 +174,43 @@ main() {
 		| sort -V \
 		| tail -n 1 \
 		| sed -e "s|-||g")
-	pushd "${repo_dir}/virtual/ot-sources-lts"
+	pushd "${repo_dir}/virtual/ot-sources-lts" >/dev/null 2>&1
 		local latest_lts_ebuild_timestamp=$(ls -1 \
 			| grep -e "\.ebuild$" \
 			| sort -V \
 			| tail -n 1 \
 			| sed -e "s|ot-sources-lts-||" -e "s|\.ebuild$||")
 
+		local bumped=0
 		if [[ "${latest_lts_ebuild_timestamp}" != "${current_lts_timestamp}" ]] ; then
-			echo "Auto bumping ot-sources-lts-${latest_lts_ebuild_timestamp}.ebuild -> ot-sources-lts-${current_lts_timestamp}.ebuild"
 			if [[ "${DRY_RUN}" != "1" ]] ; then
 				cp -a \
 					"${repo_dir}/virtual/ot-sources-lts/ot-sources-lts-${latest_lts_ebuild_timestamp}.ebuild" \
 					"${repo_dir}/virtual/ot-sources-lts/ot-sources-lts-${current_lts_timestamp}.ebuild"
 			fi
-		fi
 
-		for slot in ${LTS_SLOTS[@]} ; do
-			pushd "${repo_dir}/sys-kernel/ot-sources/"
-				local ver=$(ls -1 "ot-sources-${slot}."*".ebuild" | sort -V | tail -n 1)
-			popd
-			local lineno=$(grep -n "~sys-kernel/ot-sources-${slot}" "${repo_dir}/virtual/ot-sources-lts/"*"ebuild" | cut -f 1 -d ":")
-			if [[ "${DRY_RUN}" != "1" ]] ; then
-				sed -i -e "${lineno}d" "${repo_dir}/virtual/ot-sources-lts/"*"ebuild"
-				sed -i -e "${lineno}i ~sys-kernel/ot-sources-${ver}" "${repo_dir}/virtual/ot-sources-lts/"*"ebuild"
-			fi
-		done
-		pushd "${repo_dir}/virtual/ot-sources-lts" || popd
-			local ebuild_path=$(ls -1 *.ebuild)
-			ebuild "${ebuild_path}" digest
-		popd
-	popd
+			pushd "${repo_dir}/virtual/ot-sources-lts" >/dev/null 2>&1
+				echo "Autoediting ot-sources-lts-${current_lts_timestamp}.ebuild"
+				for slot in ${LTS_SLOTS[@]} ; do
+					pushd "${repo_dir}/sys-kernel/ot-sources/" >/dev/null 2>&1
+						local ver=$(ls -1 "ot-sources-${slot}."*".ebuild" | sort -V | tail -n 1)
+					popd >/dev/null 2>&1
+					if [[ "${DRY_RUN}" != "1" ]] ; then
+						local lineno=$(grep -n "~sys-kernel/ot-sources-${slot}" "ot-sources-lts-${current_lts_timestamp}.ebuild" | cut -f 1 -d ":")
+						sed -i -e "${lineno}d" "ot-sources-lts-${current_lts_timestamp}.ebuild"
+						sed -i -e "${lineno}i ~sys-kernel/ot-sources-${ver}" "ot-sources-lts-${current_lts_timestamp}.ebuild"
+					fi
+				done
+
+				echo "Auto bumping ot-sources-lts-${latest_lts_ebuild_timestamp}.ebuild -> ot-sources-lts-${current_lts_timestamp}.ebuild"
+				if [[ "${DRY_RUN}" != "1" ]] ; then
+					ebuild $(ls -1 *.ebuild | sort -V | tail -n 1) digest
+					git add *
+					git commit -m "Autobumped ot-sources-lts-${current_lts_timestamp}.ebuild"
+				fi
+			popd >/dev/null 2>&1
+		fi
+	popd >/dev/null 2>&1
 }
 
 main
