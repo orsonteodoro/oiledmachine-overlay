eadffa63b4c7b1e11d9349f98eb73e860777095f
Orson Teodoro <orsonteodoro@hotmail.com>
2024-02-24 10:12:20 -0800

    Add autobump support for openxr ebuild

---
+++ media-libs/openxr/autobump/get_latest_patch_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+get_latest_patch_version() {
+	local ver="${1}"
+	git ls-remote --tags "https://github.com/KhronosGroup/OpenXR-SDK-Source.git" \
+		| grep "/release-${ver}" \
+		| sed -e "s|.*/||g" \
+		| sed -r -e "s|[\^]\{\}||g" -e "s|release-||g" \
+		| sort -V \
+		| uniq \
+		| tail -n 1
+}
+
+get_latest_patch_version ${1}
