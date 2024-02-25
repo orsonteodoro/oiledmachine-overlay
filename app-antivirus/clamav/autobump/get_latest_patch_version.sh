93250f5898812deea7a72ce2fa9ca1f8fca58979
Orson Teodoro <orsonteodoro@hotmail.com>
2024-02-24 16:47:49 -0800

    Add autobump support for clamav ebuild

---
+++ app-antivirus/clamav/autobump/get_latest_patch_version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+get_latest_patch_version() {
+	local ver="${1}"
+	git ls-remote --tags "https://github.com/Cisco-Talos/clamav.git" \
+		| grep -E -e "/(CLAMAV_${ver/./_}|clamav-${ver})" \
+		| sed -r \
+			-e "s|.*/CLAMAV_||g" \
+			-e "s|.*/clamav-||g" \
+			-e "s|[\^]\{\}||g" \
+			-e "s|-rc$|_rc|g" \
+			-e "s|-rc([0-9])|_rc\1|g" \
+			-e "s|-beta|_beta|g" \
+			-e "s|@|_p|g" \
+			-e "s|([0-9])rc([0-9])|\1_rc\2|g" \
+			-e "s|([0-9])rc$|\1_rc|g" \
+			-e "s|([0-9])_([0-9]{2})|\1.\2|g" \
+			-e "s|RC|_rc|g" \
+			-e "s|090|0.90|g" \
+			-e "s|([0-9])beta|\1_beta|g" \
+			-e "/dmgxar/d" \
+			-e "/[0-9]{4}/d" \
+			-e "s|([0-9])rc|\1_rc|g" \
+		| sed -e "/_rc/d" -e "/_beta/d" -e "/sf/d" \
+		| sort -V \
+		| uniq \
+		| tail -n 1
+}
+
+get_latest_patch_version ${1}
