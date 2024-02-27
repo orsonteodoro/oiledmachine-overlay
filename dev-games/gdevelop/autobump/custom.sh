a380bba175d53f2b16041797d0678ba26225ad9a
Orson Teodoro <orsonteodoro@hotmail.com>
2024-02-26 22:48:32 -0800

    Update autobump for gdevelop ebuild for proper sorting of _beta and final versions

--- dev-games/gdevelop/autobump/custom.sh
+++ dev-games/gdevelop/autobump/custom.sh
@@ -5,9 +5,11 @@ get_latest_patch_version() {
 	git ls-remote --tags "https://github.com/4ian/GDevelop.git" \
 		| grep -e "/v${ver//./\\.}\." \
 		| sed -e "s|.*/||g" \
+		| sed -r -e "s|^v||g" -e "s|-beta([0-9])|_beta\1|" \
+		| sed -r -e "s|\.([0-9]+)$|.\1_z|g" \
 		| sort -V \
 		| uniq \
-		| sed -r -e "s|^v||g" -e "s|-beta([0-9])|_beta\1|" \
+		| sed -r -e "s|_z$||g" \
 		| tail -n 1
 }
 
