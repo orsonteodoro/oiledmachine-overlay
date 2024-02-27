bc8cd0bf7f5bca51ba580a7dc74084b8c85fbf8a
Orson Teodoro <orsonteodoro@hotmail.com>
2024-02-26 16:05:09 -0800

    Add autobump support for wrapt ebuild

---
+++ dev-python/wrapt/autobump/get_latest_patch_version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+get_latest_patch_version() {
+	local ver="${1}"
+	git ls-remote --tags "https://github.com/GrahamDumpleton/wrapt" \
+		| grep "refs/tags/" \
+		| grep -e "/${ver}" \
+		| sed -e "s|.*/||g" \
+		| sed -r -e "s|[\^]\{\}||g" \
+		| sed -r -e "s|([0-9])rc|\1_rc|g" \
+		| sed -r -e "s|(\.[0-9]+)$|\1_z|g" \
+		| sort -V \
+		| uniq \
+		| sed -r -e "s|_z$||g" \
+		| tail -n 1
+}
+
+get_latest_patch_version ${1}
