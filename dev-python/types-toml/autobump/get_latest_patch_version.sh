acf59dab52aadb0e5b62249b49e58549a65bb37b
Orson Teodoro <orsonteodoro@hotmail.com>
2024-02-26 16:33:46 -0800

    Add autobump support for types-toml ebuild

---
+++ dev-python/types-toml/autobump/get_latest_patch_version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+get_latest_patch_version() {
+	local ver="${1}"
+	wget -q -O - "https://pypi.org/rss/project/types-toml/releases.xml" \
+		| grep "title" \
+		| tail -n +2 \
+		| cut -f 2 -d ">" \
+		| cut -f 1 -d "<" \
+		| sort -V \
+		| grep "^${ver}" \
+		| tail -n 1
+}
+
+get_latest_patch_version ${1}
