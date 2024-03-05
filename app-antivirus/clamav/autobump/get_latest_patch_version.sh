ac6362af5da7c6558f293f5231daac4b9614417e
Orson Teodoro <orsonteodoro@hotmail.com>
2024-03-05 14:41:47 -0800

    Fix version sorting for autobump for clamav ebuild

--- app-antivirus/clamav/autobump/get_latest_patch_version.sh
+++ app-antivirus/clamav/autobump/get_latest_patch_version.sh
@@ -7,23 +7,33 @@ get_latest_patch_version() {
 			-e "s|.*/CLAMAV_||g" \
 			-e "s|.*/clamav-||g" \
 			-e "s|[\^]\{\}||g" \
-			-e "s|-rc$|_rc|g" \
-			-e "s|-rc([0-9])|_rc\1|g" \
-			-e "s|-beta|_beta|g" \
+			-e "s|-|_|g" \
 			-e "s|@|_p|g" \
-			-e "s|([0-9])rc([0-9])|\1_rc\2|g" \
-			-e "s|([0-9])rc$|\1_rc|g" \
-			-e "s|([0-9])_([0-9]{2})|\1.\2|g" \
 			-e "s|RC|_rc|g" \
 			-e "s|090|0.90|g" \
+			-e "s|([0-9])rc([0-9]+)|\1_rc\2|g" \
+			-e "s|([0-9])rc$|\1_rc|g" \
+			-e "s|([0-9])_([0-9]{2})|\1.\2|g" \
 			-e "s|([0-9])beta|\1_beta|g" \
 			-e "/dmgxar/d" \
 			-e "/[0-9]{4}/d" \
 			-e "s|([0-9])rc|\1_rc|g" \
 		| sed -e "/sf/d" \
-		| sed -E -e "s|\.([0-9])$|.\1_z|g" \
+		| sed -e "/2008/d" \
+		| sed -E -e "s|_rc_|_rc0_|g" \
+		| sed -E -e "s|_rc$|_rc0|g" \
+		| sed -E -e "s|_rc|_mrc|g" \
+		| sed -E -e "s|^([0-9]+\.[0-9]+\.[0-9]+)$|\1_o|g" \
+		| sed -E -e "s|^([0-9]+\.[0-9]+)_|\1,|g" \
+		| sed -E -e "s|^([0-9]+\.[0-9]+)$|\1,,|g" \
 		| sort -V \
 		| uniq \
+		| sed -E -e "s|^([0-9]+\.[0-9]+),,$|\1|g" \
+		| sed -E -e "s|^([0-9]+\.[0-9]+),|\1_|g" \
+		| sed -E -e "s|^([0-9]+\.[0-9]+\.[0-9]+)_o$|\1|g" \
+		| sed -E -e "s|_mrc|_rc|g" \
+		| sed -E -e "s|_rc0$|_rc|g" \
+		| sed -E -e "s|_rc0_|_rc_|g" \
 		| sed -e "s|_z$||g" \
 		| tail -n 1
 }
