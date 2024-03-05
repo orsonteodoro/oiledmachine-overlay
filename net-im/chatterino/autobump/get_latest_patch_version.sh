3a0ecaf880f98d7cf775a55286e91c26c9057db7
Orson Teodoro <orsonteodoro@hotmail.com>
2024-03-05 11:44:31 -0800

    Add autobump support for chatterino ebuild

---
+++ net-im/chatterino/autobump/get_latest_patch_version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+get_latest_patch_version() {
+	local ver="${1}"
+	git ls-remote --tags "https://github.com/Chatterino/chatterino2.git" \
+		| grep "refs/tags/" \
+		| grep -E -e "/v${ver//./\\.}\." \
+		| sed -e "s|.*/||g" \
+		| sed -r -e "s|^v||g" -e "s|[\^]\{\}||g" \
+		| sed -e "s|-|_|g" \
+		| sort -V \
+		| uniq \
+		| tail -n 1
+}
+
+get_latest_patch_version ${1}
