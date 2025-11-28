973e7d2bee6fc059a46215ac3975c627dffa93a3
Orson Teodoro <orsonteodoro@hotmail.com>
2025-11-28 08:33:28 -0800

    Update rpath_verify.sh

--- scripts/rpath_verify.sh
+++ scripts/rpath_verify.sh
@@ -8,13 +8,16 @@ IFS=$' \t\n'
 for x in "${L[@]}" ; do
 	[[ -L "${x}" ]] && continue
 	if ldd "${x}" 2>/dev/null | grep -q "re2" ; then
-		ldd "${x}" 2>/dev/null | grep "/usr/lib/re2" || echo "RPATH broken for ${x}"
+		ldd "${x}" 2>/dev/null | grep -q "/usr/lib/re2" || echo "RPATH is broken for ${x}"
 	fi
 	if ldd "${x}" 2>/dev/null | grep -q "libabsl_" ; then
-		ldd "${x}" 2>/dev/null | grep "/usr/lib/abseil-cpp" || echo "RPATH broken for ${x}"
+		ldd "${x}" 2>/dev/null | grep -q "/usr/lib/abseil-cpp" || echo "RPATH is broken for ${x}"
 	fi
 	if ldd "${x}" 2>/dev/null | grep -q "grpc" ; then
-		ldd "${x}" 2>/dev/null | grep "/usr/lib/grpc" || echo "RPATH broken for ${x}"
+		ldd "${x}" 2>/dev/null | grep -q "/usr/lib/grpc" || echo "RPATH is broken for ${x}"
+	fi
+	if ldd "${x}" 2>/dev/null | grep -q "libavcodec.so" ; then
+		ldd "${x}" 2>/dev/null | grep -q "/usr/lib/ffmpeg" || echo "RPATH is possibily missing or would benefit with multislot ffmpeg for ${x}"
 	fi
 	if ldd "${x}" 2>/dev/null | grep -q "not found" ; then
 		echo "RPATH missing for ${x}"
