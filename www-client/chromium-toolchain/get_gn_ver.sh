a28fed2663ea74dbc437db38d8bcc22621706f6b
Orson Teodoro <orsonteodoro@hotmail.com>
2026-02-11 17:07:15 -0800

    Update chromium-toolchain ebuild to 145.0.7632.45

--- www-client/chromium-toolchain/get_gn_ver.sh
+++ www-client/chromium-toolchain/get_gn_ver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 main() {
-	# Commit from https://github.com/chromium/chromium/blob/144.0.7559.59/DEPS#L506
-	GN_COMMIT=${GN_COMMIT:-"6e0b557db44b3c164094e57687d20ba036a80667"}
+	# Commit from https://github.com/chromium/chromium/blob/145.0.7632.45/DEPS#L506
+	GN_COMMIT=${GN_COMMIT:-"5550ba0f4053c3cbb0bff3d60ded9d867b6fa371"}
 	echo "GN_COMMIT: ${GN_COMMIT}"
 
 	if ! [[ -e "gn" ]] ; then
